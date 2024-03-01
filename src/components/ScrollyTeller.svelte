0096ff5ffb1c5082e2f4189ec2d2d58591f46b74
JasonDai1219 <114436475+JasonDai1219@users.noreply.github.com>
2024-02-29 19:05:00 -0800

    Update ScrollyTeller.svelte

--- src/components/ScrollyTeller.svelte
+++ src/components/ScrollyTeller.svelte
@@ -113,6 +113,16 @@
     padding: 2em;
     margin: 0 0 2em 0;
   }
+  .background-info {
+    font-size:20px;
+    padding:10px;
+  }
+  .reminder {
+    bottom:0;
+    font-size:25px;
+    margin-top:100px;
+    color:red;
+  }
 
 </style>
 
@@ -138,7 +148,16 @@
   </div>
 
   <div class="foreground" slot="foreground">
-    <section>Please maximize your browser window for the best viewing experience. hi</section>
+    <section>This is the first section.
+      <div class="background-info">
+        On April 15, 1912, the RMS Titanic, a marvel of early 20th-century engineering, embarked on its maiden voyage from Southampton to New York City. However, this journey was tragically cut short after the ship collided with an iceberg in the North Atlantic. This disaster resulted in the loss of over 1,500 lives, making it one of the deadliest peacetime maritime disasters in history. The Titanic's sinking has since captivated the public imagination, leading to numerous investigations, studies, and narratives that seek to understand the circumstances and decisions that led to such high mortality. Factors such as social class, gender, age, and access to lifeboats have been extensively analyzed to comprehend the disparities in survival rates among the passengers.
+      </div>
+      <div class="background-info">
+        This visualization project aims to delve into the intricate dynamics of survival aboard the Titanic, focusing on how various factors influenced the chances of surviving the disaster. Our research question centers on comparing the survival rates across different demographics and circumstances, including gender, passenger class (Pclass), fare price, availability of lifeboats, and age. By examining these variables, we seek to uncover patterns and insights that can provide a deeper understanding of the social, economic, and logistical elements that played a critical role in determining who survived and who perished in this tragic event. Through this analysis, we aspire to contribute to the ongoing discussion about the Titanic, offering a nuanced perspective on how different factors intersected to affect survival chances on that fateful night.
+      <div class="reminder">
+        Please Full Screen to Gain Maximized Experience
+      </div>
+      </section>
     <section>This is the second section.</section>
     <section>This is the third section.</section>
     <section>This is the fourth section.</section>
